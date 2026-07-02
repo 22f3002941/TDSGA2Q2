c6c60aa3aafb15ed24ccdc4d715e3e001dcaf63a
Vasudevan Rajagopalan <vasudevan.rajagopalan@gmail.com>
2026-07-02 09:20:46 +0530

    Implement config precedence service - bugfix2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],   # If the assignment specifies a particular origin, use that instead.
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
